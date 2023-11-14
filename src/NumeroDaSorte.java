287331a0f68721a081ae346e69e73bed7f796554
Eduardo Martins <edu.almeida.01@hotmail.com>
2023-11-13 23:01:14 -0300

    Raname class

--- src/NumeroDaSorte.java
+++ src/NumeroDaSorte.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class PrimeiraClasse {
+public class NumeroDaSorte {
     public static void main(String[] args){
 
         int numeroSecreto = new Random().nextInt(100);
